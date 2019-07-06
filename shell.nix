51552aa460e7dcd316ee0f26582330a8ca5d7200
andylokandy <andylokandy@hotmail.com>
2019-07-06 17:50:19 +0800

    Use the rust nightly channel

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  rustStableChannel = nixpkgs.latest.rustChannels.stable.rust.override {
+  rustNightlyChannel = nixpkgs.latest.rustChannels.nightly.rust.override {
     extensions = [
       "rust-src"
       "rls-preview"
@@ -14,6 +14,6 @@ with nixpkgs;
   stdenv.mkDerivation {
     name = "moz_overlay_shell";
     buildInputs = [
-      openssl pkgconfig rustStableChannel
+      openssl pkgconfig rustNightlyChannel
     ];
   }
